c6beabca23680da94dc823f1dd2a60165153c2bc
Nssienphilip <95418182+12philip34@users.noreply.github.com>
2022-09-15 13:11:20 +0100

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import "./style.css"
+import playMusic from "./music.mp3";
+import useSound from "use-sound";
+
+function Counter() {
+    const [letsPlay] = useSound(playMusic)
+    let [num, setNum] = useState(0);
+
+    useEffect(() => {
+    
+        letsPlay();
+      }, [letsPlay]);
+
+    let incNum = () => {
+        setNum(num + 1)
+    }
+
+    let decNum = () => {
+        if(num > 0){
+            setNum(num -1)
+        }
+    }
+
+    let handleChange = (e) => {
+            setNum(e.target.value);
+    }
+    return (
+        <div>
+            <section>
+                <div className="box">
+                    <button onClick={incNum}>+</button>
+                    <input value={num} onChange={handleChange} />
+                    <button onClick={decNum}>-</button>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Counter
